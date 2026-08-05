1c93dfa74c90cda8bd00c402f7dfe463df52d796
JIN <jgw00518@gmail.com>
2026-08-05 18:16:08 +0900

    260805

---
+++ Stateful/material3_design_app/lib/util/color_spec.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+
+// 아래와 같이 Material3 Design을 쓰지 않는다면 이런식으로 class를 만들어서 사용 (Material3 사용 추천)
+
+class Colorspec {
+  static Color primaryColor = Colors.red;
+}
